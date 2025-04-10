8882e5bcbeb07f0babd793e39879e1034d74ca5d
DANNYWW001 <lekandaniel005@gmail.com>
2025-04-10 16:08:06 +0100

    car  commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import Experience from "./components/Experience";
+import Featured from "./components/Featured";
+import HowItWorks from "./components/HowItWorks";
+import Reviews from "./components/Reviews";
+import Rent from "./components/Rent";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <div className="hero h-screen max-h-[689px] bg-no-repeat bg-cover">
+        <Nav />
+        <Hero />
+      </div>
+      <Experience />
+      <Featured />
+      <HowItWorks />
+      <Reviews />
+      <Rent />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
